bd34a509155472ea005bef56588da2247cf96680
Juliano dos Santos <141170164+santosjuliano02@users.noreply.github.com>
2023-10-18 08:47:34 -0300

    comitando

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 70.5
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if (velocidade > 60) {
+    console.log('Você ultrapasou a velocidade. Multado!')
+    console.log('Na proxima, preste mais atençao nos radares.')
+}
