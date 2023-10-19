f8ad77aad90a26d84b331f44d6d3da86d21d6e07
Juliano dos Santos <141170164+santosjuliano02@users.noreply.github.com>
2023-10-19 19:16:25 -0300

    desafio

---
+++ aula011/ex008b.js
@@ -0,0 +1,11 @@
+var velocidade = 0
+
+console.log(` A velocidade do seu carro é de ${velocidade} Km/h. `)
+
+if (60 < velocidade) {
+    console.log(`Você ultrapassou a Velocidade permitida. Multado!`)
+}
+
+else {
+    console.log(` Dirija sempre usando o cinto de segurança!`)
+}
---
+++ aula011/ex009b.js
@@ -0,0 +1,16 @@
+var pais = 'Brasil'
+var pais2 = 'EUA'
+
+if (pais == 'BRASIL', 'Brasil', 'brasil') {
+    console.log(`Vivendo em ${pais}`)
+    console.log('Você é Brasileiro!')
+}
+
+if (pais2 == 'EUA', 'Eua', 'eua') {
+    console.log(`Vvendo em ${pais2}`)
+    console.log('Você é Americano!')
+}
+
+else {
+    console.log('Você é Estrangeiro!')
+}
